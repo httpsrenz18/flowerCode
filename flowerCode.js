401ec96c0cc05a53c2d287740751742f28d90574
httpsrenz18 <ramosreniel16@gmail.com>
2024-10-02 15:28:49 +0800

    Update flowerCode.js

--- flowerCode.js
+++ flowerCode.js
@@ -6,17 +6,13 @@ onload = () => {
 };
 
 
-// Select the audio element
 const audio = document.getElementById('audio');
 
-// Function to play the audio
 function playAudio() {
     audio.play();
 }
 
 
-
-// Optional: Add event listeners for any specific controls or interactions
 document.addEventListener('DOMContentLoaded', () => {
     // Example: Automatically play audio when page loads
     // playAudio();
